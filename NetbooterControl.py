4a5bc02303e2395533111ac0d0bf8bccf610350e
Unknown <jonyorker@gmail.com>
2017-07-05 14:05:44 -0400

    First test for pshow

--- NetbooterControl.py
+++ NetbooterControl.py
@@ -4,4 +4,19 @@ import telnetlib
 with open('config.json') as config:
     data = json.load(config)
 
-print (data['PS1']['ip'])
+HOST = data['PS1']['ip']
+USER = data['PS1']['username']
+PASSWORD = data['PS1']['password']
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("login: ")
+tn.write(USER + "\n")
+if PASSWORD:
+    tn.read_until("Password: ")
+    tn.write(PASSWORD + "\n")
+
+tn.write("pshow")
+tn.write("exit\n")
+
+print (tn.read_all())
