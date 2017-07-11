3a0af43b0e956d82022e56c690487a1fcda071cd
Unknown <jonyorker@gmail.com>
2017-07-11 14:23:42 -0400

    Email works, also added code of when it attaches a null to the login

--- NetbooterControl.py
+++ NetbooterControl.py
@@ -12,12 +12,22 @@ import time
 import sys
 import smtplib
 
+from email.message import EmailMessage
+
 ### Set Variables
 HOST = str(sys.argv[1])			            # The remote host IP address
 COMMAND_USER = str(sys.argv[2])                     # The command we wish to send
 PORT = int(23)       				    # The server port number - telnet better than http
 USER = str('admin')
 PASS = str('Rain4Est!')
+
+### Initialize email variables
+global msg
+msg = EmailMessage()
+msg.set_content('No results yet')
+msg['From'] = 'mdmail@ciena.com'
+msg['To'] = 'alounsbu@ciena.com'
+
 
 ### Establish if command if on or off
 
@@ -31,48 +41,35 @@ else :
 ### Define Function
 
 def Comm_Function(COMMAND):
+    global msg
     cmd = ('$'+COMMAND+'\r').encode('utf-8')
     sock.send(cmd)                                  #report status command
     time.sleep(1)                                   #Wait for command to complete
     recv_status = sock.recv(2048)                   #Receive output
     if recv_status.endswith(b'$A0'):
-                                                    #Set message for email
-        MESSAGE = str("""From: mdmail.ciena.com <mdmail.ciena.com> 
-        To: alounsbu@ciena.com <alounsbu@ciena.com>
-        Subject: Operation Successful
-
-        Operation Successful.
-        """)
+        msg.set_content('Operation Successful.')     #Set message for email
+        msg['Subject'] = 'Operation Successful'
                                       
     else:                                           #Try again in case delay wasn't enough
         time.sleep(5)                               #Wait for command to complete
         recv_status = sock.recv(2048)               #Receive output
         if recv_status.endswith(b'$A0'):
-                                                        #Set message for email
-            MESSAGE = str("""From: mdmail.ciena.com <mdmail.ciena.com> 
-            To: alounsbu@ciena.com <alounsbu@ciena.com>
-            Subject: Operation Successful
-
-            Operation Successful.
-            """)
+            msg.set_content('Operation Successful.') #Set message for email
+            msg['Subject'] = 'Operation Successful'
         else:
-                                                        #Set message for email
-            MESSAGE = str("""From: mdmail.ciena.com <mdmail.ciena.com> 
-            To: alounsbu@ciena.com <alounsbu@ciena.com>
-            Subject: Operation Failed
+            msg.set_content('Operation Failed.')     #Set message for email
+            msg['Subject'] = 'Operation Failed'
 
-            Operation Failed.
-            """)
-    return MESSAGE
+    return 
 
 ### Establish Connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-time.sleep(0.1) 		                    #use time.sleep to give delay and netBooter time to process
+time.sleep(0.5) 		                    #use time.sleep to give delay and netBooter time to process
 	
 sock.send(b'\r')				    #send \r to start at beginning of line
-time.sleep(0.5)
+time.sleep(1)
 
 cmd = ('$a1 '+USER+' '+PASS+'\r').encode('utf-8')
 sock.send(cmd)		                            #login command
@@ -82,35 +79,29 @@ time.sleep(1)					    #delay between commands to allow NP unit to process
 recv = sock.recv(2048)                              #Receive output
 
 if recv.endswith(b'$A0'):
-    MESSAGE = Comm_Function(COMMAND)
+    m = Comm_Function(COMMAND)
+elif recv.endswith(b'$A0\x00'):
+    m = Comm_Function(COMMAND)
 elif recv.endswith(b'$AF'):
-                                                    #Set message for email
-    MESSAGE = str("""From: mdmail.ciena.com <mdmail.ciena.com> 
-    To: alounsbu@ciena.com <alounsbu@ciena.com>
-    Subject: Failed to Connect
-
-    Failed to Connect.
-    """)
+    msg.set_content('Failed to Connect.')            #Set message for email
+    msg['Subject'] = 'Failed to Connect'                                               
 else:
     time.sleep(5)                                   #Sleep some more in case of slow communication
-    MESSAGE = Comm_Function(COMMAND)
+    m = Comm_Function(COMMAND)
     
 ### Close connection
 	
-time.sleep(0.1)
+time.sleep(0.5)
 	
 sock.close()
 
-print (MESSAGE)
 ### Send Email
-
-SENDER = 'mdmail.ciena.com'
-RECEIVER = 'alounsbu@ciena.com'
-
 smtpObj = smtplib.SMTP('mdmail.ciena.com')
+print (msg)
+
 try:
-    smtpObj.sendmail(SENDER, RECEIVER, MESSAGE)
+    smtpObj.send_message(msg)
     print ("Successfully sent email")
-except SMTPException:
+except SMTPException as exception:
     print ("Error: unable to send email")
 	
